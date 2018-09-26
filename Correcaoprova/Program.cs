2549d542093d0657784cdda215d2add291e47fb2
Marcelo Haziel <bioondorxrx@gmail.com>
2018-09-26 18:22:42 -0300

    v1.0
    Primeiro Commit do Projeto

---
+++ Correcaoprova/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Correcaoprova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                String nomeProduto;
+                int quantidade;
+                double valorProduto, valorTotal;
+
+                Console.WriteLine("Digite o nome do produto");
+                nomeProduto = Console.ReadLine();
+
+                Console.WriteLine("Digite o valor do produto: ");
+                valorProduto = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite a quantidade :");
+                quantidade = Convert.ToInt32(Console.ReadLine());
+
+                valorTotal = quantidade * valorProduto;
+
+                Console.Clear();
+                Console.WriteLine(" ######### Venda de Produtos #########");
+                Console.WriteLine("Produto: " + nomeProduto);
+                Console.WriteLine("Qtde: " + quantidade + "x Valor Unitário:" + valorProduto);
+                //.ToString()- converte o número para texto, o "C" formata para moeda.
+                Console.WriteLine("Total de Venda: " + valorTotal.ToString("C"));
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.Write("Dgite corretamente");
+            }
+            Console.ReadKey();
+            }
+        }
+    }
+}
